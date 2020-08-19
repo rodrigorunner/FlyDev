bd91d648ccb3e98709f99261b0ca5d512f4067e2
rodrigorunner <rodrigoregisrun@gmail.com>
2020-08-19 11:54:42 -0300

    Script Saudações

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Exemplo de um script.
+
+// O objetivo do script é identificar o horário
+// e enviar saudações.
+
+let time = 19;
+
+if(time <= 12) {
+    alert('Good Morning');
+} else if(time <= 18) {
+    alert('Good Afternoon');
+} else {
+    alert('Good Evening');
+}
